338c99ec432ba93fe5922816202551ffa338c97c
CirnoV (Sickle) <cirnov59@gmail.com>
2024-09-19 21:38:28 +0900

    불필요한 파일 제거

--- tests/__fixtures__/output/parameters/channelKey.ts
+++
@@ -1,6 +0,0 @@
-/**
- * ### 채널 키
- * 콘솔에서 표시되는 채널 키입니다.
- * `pgProvider` 파라미터가 없는 경우에 필수로 존재해야 합니다. 두 파라미터가 모두 존재하는 경우 `channelKey`를 적용하니 둘 중 하나만 제공해주세요.
- */
-export type channelKey = string;
--- tests/__fixtures__/output/parameters/index.ts
+++
@@ -1,2 +0,0 @@
-export { storeId } from "./storeId.js";
-export { channelKey } from "./channelKey.js";
