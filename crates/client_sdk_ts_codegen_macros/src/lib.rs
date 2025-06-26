76d3ff1f6ec19536e046c136df642c059e09b39f
CirnoV <cirnov59@gmail.com>
2025-06-26 20:41:40 +0900

    Enum 타입 참조에 대한 TypeScript 코드 생성 수정

--- crates/client_sdk_ts_codegen_macros/src/lib.rs
+++ crates/client_sdk_ts_codegen_macros/src/lib.rs
@@ -41,7 +41,9 @@ fn expand(input: proc_macro::TokenStream, mode: MacroMode) -> proc_macro::TokenS
         _as: _,
         output_type,
         args,
-    } = syn::parse::<TsParseInput>(input).unwrap();
+    } = syn::parse::<TsParseInput>(input).expect(
+        "Failed to parse ts_parse! macro input. Expected format: \"source code\" as OutputType",
+    );
 
     let source = match mode {
         MacroMode::TsParse => source.into_token_stream(),
@@ -52,23 +54,42 @@ fn expand(input: proc_macro::TokenStream, mode: MacroMode) -> proc_macro::TokenS
     };
 
     quote! {{
+        use biome_console::{
+            fmt::{Formatter, Termcolor},
+            markup,
+        };
+        use biome_diagnostics::{DiagnosticExt, PrintDiagnostic};
         use biome_js_syntax::*;
         use biome_rowan::AstNode;
-        let node = biome_js_parser::parse(
-            indoc::formatdoc!(#source #args).as_str(),
+        let formatted_source = indoc::formatdoc!(#source #args);
+        let parsed = biome_js_parser::parse(
+            formatted_source.as_str(),
             biome_js_syntax::JsFileSource::ts(),
             biome_js_parser::JsParserOptions::default(),
         );
-        if node.has_errors() {
-            let error = node
-                .into_diagnostics()
-                .into_iter()
-                .map(|d| biome_diagnostics::print_diagnostic_to_string(&d.into()))
-                .collect::<Vec<_>>()
-                .join("\n");
-            panic!("Error parsing ts: {}", error);
+        let diagnostics = parsed.diagnostics();
+        if !diagnostics.is_empty() {
+            let mut diagnostics_buffer = termcolor::Buffer::no_color();
+
+            let termcolor = &mut Termcolor(&mut diagnostics_buffer);
+            let mut formatter = Formatter::new(termcolor);
+
+            for diagnostic in diagnostics {
+                let error = diagnostic.clone().with_file_source_code(&formatted_source);
+
+                formatter
+                    .write_markup(markup! {
+                        {PrintDiagnostic::verbose(&error)}
+                    })
+                    .expect("failed to emit diagnostic");
+            }
+
+            let formatted_diagnostics = std::str::from_utf8(diagnostics_buffer.as_slice())
+                .expect("non utf8 in error buffer");
+
+            panic!("{}", formatted_diagnostics);
         }
-        node.syntax()
+        parsed.syntax()
             .descendants()
             .find_map(#output_type::cast)
             .unwrap()
